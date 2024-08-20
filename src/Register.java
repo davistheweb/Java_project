b66b4970ef9451345a6cf399e4e7c30b1ec18bdd
davistheweb <smartkelvin775@gmail.com>
2024-08-20 08:30:23 +0100

    :

---
+++ src/Register.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Register {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextArea textArea1;
+    private JTextField textField3;
+    private JComboBox comboBox1;
+    private JTextField textField4;
+    private JComboBox comboBox2;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
